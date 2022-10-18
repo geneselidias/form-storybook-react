4751590c45c808f5588ea22019b603f19ac8ab39
Geneseli Dias <geneselidias@gmail.com>
2022-10-18 19:33:45 -0300

    adding components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
+import { Heading } from './components/Heading';
+import { Text } from './components/Text';
+import { Logo } from './Logo';
+ 
 import './styles/global.css';
 
 export function App() {
   return (
-    <>
-      <h1 className="font-bold text-2xl text-violet-500">Hello World</h1>
+    <div className="w-screen h-screen bg-gray-900 flex items-center justify-center text-gray-100">
+      <Logo />
 
-      <button className="bg-cyan-500 font-medium px-4 py-2 rounded text-white hover:bg-cyan-300">
-        Enviar</button>
-    </>
+      <Heading size="lg">
+        Ignite  Lab
+      </Heading>
+
+      <Text size="lg" className="text-gray-400">Faça login e comece a usar!</Text>
+
+    </div>
   )
 }
